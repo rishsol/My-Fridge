65d07b72176642b9a099dbfa7de3001d024d0149
rishsol <rishabsolanki09@gmail.com>
2020-05-25 23:40:30 -0400

    using forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ js/script.js
@@ -0,0 +1,5 @@
+var i = 0;
+var original = document.getElementById("grid");
+function duplicate() {
+    print(4)
+}
